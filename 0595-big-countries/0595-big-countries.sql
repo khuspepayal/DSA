15c4f0926a563995ee955a9962163925d6d0e132
khuspepayal <khuspepayal2003@gmail.com>
2026-06-26 22:27:52 +0530

    Time: 264 ms (100%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0595-big-countries/0595-big-countries.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT NAME,POPULATION,AREA
+FROM WORLD
+WHERE AREA>=3000000 OR POPULATION>=25000000;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
