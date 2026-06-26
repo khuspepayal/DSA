43bb7ace50d222100cc176c866f47238fb49d168
khuspepayal <khuspepayal2003@gmail.com>
2026-06-26 22:28:26 +0530

    Time: 237 ms (99.96%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT *
+FROM cinema
+WHERE id%2=1 and description!="boring"
+order by rating desc;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
