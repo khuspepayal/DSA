009fff089f924aee7dbcaca8a505262c89e9189c
payal4751 <khuspepayal2003@gmail.com>
2026-06-28 12:02:45 +0530

    Time: 531 ms (7.01%), Space: 47 MB (27.77%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count==1){
+            return nums[i];
+        }
+        }
+        return -1;
+}
+
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
