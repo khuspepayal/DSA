9ea58b6a604731c5067c176671c473679814a01e
payal4751 <khuspepayal2003@gmail.com>
2026-07-02 12:32:19 +0530

    Time: 1 ms (78.29%), Space: 42.1 MB (88.97%) - LeetHub - Time: O(logn), Space: O(logn)

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set=new HashSet<>();
+        while(n!=1){
+            if(set.contains(n)){
+                return false;
+            }
+            else{
+                set.add(n);
+                n=getSum(n);
+            }
+        }
+        return true;
+
+    }
+
+    public int getSum(int n) {
+        int sum=0;
+        while(n>0){
+            int digit=n%10;
+            sum+=digit*digit;
+            n/=10;
+        }
+        return sum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
